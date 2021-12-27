23a048783d3831d9252eb89aa7e00df235689c1a
WorkWithSoham <sohamtembe98@gmail.com>
2021-12-27 02:01:43 -0600

    Login Authentication implemented

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,22 +1,79 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const { UserInputError } = require('apollo-server');
+const { UserInputError } = require("apollo-server");
+const {
+  validateRegisterInputs,
+  validateLoginInput,
+} = require("../../utils/validators");
 
 const { SECRET_KEY } = require("../../config");
 const User = require("../../models/User");
 
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+};
+
 module.exports = {
   Mutation: {
+    // Login
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { errors });
+      }
+
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong Credentials";
+        throw new UserInputError("Wrong Credentials", { errors });
+      }
+
+      const token = generateToken(user);
+
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
+
+    // Register
     async register(
       _,
-      { registerInput: { username, email, password, confirmPassword } },
+      { registerInput: { username, email, password, confirmPassword } }
     ) {
-      const user = await User.findOne({ username })
-      if(user) {
-        throw new UserInputError('Username is taken', {
+      const { errors, valid } = validateRegisterInputs(
+        username,
+        email,
+        password,
+        confirmPassword
+      );
+      if (!valid) {
+        throw new UserInputError("Errors", { errors });
+      }
+
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is taken", {
           errors: {
-            username: 'This username is taken'
-          }
+            username: "This username is taken",
+          },
         });
       }
       password = await bcrypt.hash(password, 12);
@@ -25,26 +82,18 @@ module.exports = {
         email,
         password,
         username,
-        createdAt: new Date().toISOString()
+        createdAt: new Date().toISOString(),
       });
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
         id: res._id,
-        token
+        token,
       };
-    }
-  }
+    },
+  },
 };
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -24,7 +24,8 @@ module.exports = gql`
   }
 
   type Mutation {
-    register ( registerInput: RegisterInput): User!
+    register ( registerInput: RegisterInput): User!,
+    login (username: String!, password: String!): User!
   }
 
   type Query {
